4c60a41deca6fcc6e93dca59aa3ef469d2902cf6
eduardo-script <eduardovinicius.code@gmail.com>
2026-05-06 14:22:36 -0300

    estrutura inicial do menu e gitignore

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
-console.log('rodando...')
+const tela = require("readline-sync")
+
+function main() {
+  let loop = true
+
+  while(loop) {
+    console.log("1 - Cadastrar aluno\n2 - Adicionar Notas\n3 - Listar\n0 - Sair")
+   const opcao:string = tela.question("Deseja continuar? ")
+
+    switch(opcao) {
+      case "0":
+        loop = false
+      break
+      case "1":
+
+      break
+      case "2":
+      break
+      default
+        console.log("Opção invalida!")  
+      break
+    }      
+  }
+}
+
+main()
