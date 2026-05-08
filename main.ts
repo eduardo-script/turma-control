c8de9f8d581de01cc3b1ed6643f7f79fb05bb925
eduardo-script <eduardovinicius.code@gmail.com>
2026-05-08 16:11:16 -0300

    criando funcionalidades de notas

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 const  tela  = require("readline-sync")
 const cadastro = require("./src/cadastro-aluno")
+const notas = require("./src/cadastrar-notas")
 
 function main() {
   let loop = true
 
   while(loop) {
     console.clear()
-    console.log("1 - Cadastrar aluno\n2 - Adicionar Notas\n3 - Listar\n0 - Sair")
+    console.log("1 - Cadastrar aluno | 2 - Adicionar Notas | 3 - Listar | 0 - Sair")
    const opcao:string = tela.question("Deseja continuar? ")
 
     switch(opcao) {
@@ -17,6 +18,7 @@ function main() {
 	cadastro.telaCadastro()
       break
       case "2":
+	notas.telaCadastrarNotas()
       break
       case "3":
       	cadastro.telaListaAlunos()
--- src/cadastro-aluno.ts
+++ src/cadastro-aluno.ts
@@ -1,8 +1,15 @@
 const tela = require("readline-sync")
+const { titulo } = require("./utils")
 
+// criar uma lista de string para guardas os alunos
 let alunos:string[] = []
 
+// responsável para gerar tela de cadastros
+
+
 function telaCadastro() {
+
+  titulo("CADASTRO DE ALUNO")
   const nomeAluno:string = tela.question("Nome do aluno: ")
   const dataNascimento:string = tela.question("Data Nascimento: ")
   const contato:string = tela.question("Numero de celular: ")
@@ -14,11 +21,21 @@ function telaCadastro() {
   console.log(alunos)
 }
 
-function telaListaAlunos() {
+//responsável em carregar os alunos cadastrados
+function telaListaAlunos() { 
+	titulo("LISTA DE ALUNOS")
 	  for(let i = 0; i < alunos.length; i++) {
-    console.log (`${i + 1}. ${alunos[i]}`)
-  }
+             const partes = alunos[i]!.split(";")
+	     const nome = partes[0]
+	     const data = partes[1]
+	     const celular = partes[2]
+	     
+             console.log (`${i + 1}.Nome: ${nome} | Nasc: ${data} | Tel: ${celular}`)
+  } 
+
+tela.question("\nPressione Enter para voltar ao menu...")
+
 }
 
 // responsável de deixar a função publica
-export = { telaCadastro, telaListaAlunos }
+module.exports = { telaCadastro, telaListaAlunos }
