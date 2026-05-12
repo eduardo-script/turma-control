8d901dfe3d5ed64af39378b7194c4307fee723ec
eduardo-script <eduardovinicius.code@gmail.com>
2026-05-12 17:10:47 -0300

    att data e alunos

--- src/data.ts
+++ src/data.ts
@@ -1,17 +1,31 @@
+const fs = require("fs")
 let alunos:string[] = []
-let notas:string[][]  = []
+
+function gerarArquivo(arquivo:string) {
+  if(!fs.existsSync(arquivo)) {
+    fs.writeFileSync(arquivo, "")
+  }
+}
 
 function addAluno(nome:string, dataNascimento:string, telefone:string) {
   const id = alunos.length + 1
-  alunos.push(`${id};${nome};${dataNascimento};${telefone}`)		
+  const aluno = `${id};${nome};${dataNascimento};${telefone}`
+  alunos.push(aluno)
+  fs.appendFileSync("alunos.txt", `${alunos}\n`)
 }
 
 function listarAlunos(){
+  const arquivo 
+
 	for(let i:number = 0; i < alunos.length; i++) {
 		console.log(alunos[i])
 	}
 }
 
-function addNotas(idAluno:string, nota1:number, nota2:number,nota3:number){}
+function addNotas(idAluno: string, nota1: number, nota2: number,nota3: number){
+  const posicaoAluno = Number(idAluno) - 1
+  const aluno = alunos[posicaoAluno]
+  alunos[posicaoAluno] = `${aluno};${nota1};${nota2};${nota3}`
+}
 
 export = { addAluno, listarAlunos, addNotas }
